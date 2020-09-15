31c56cdba6b88b6b7adc3c6767bbd25ab03d7fec
Ridhigutta <70867155+Ridhigutta@users.noreply.github.com>
2020-09-14 20:01:18 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# VS_isTouching_bounceOff
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ myOwnLibrary.js
@@ -0,0 +1,28 @@
+function isTouching(object1, object2){
+    if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2
+      && object1.y - object2.y < object2.height/2 + object1.height/2
+      && object2.y - object1.y < object2.height/2 + object1.height/2) {
+    return true;
+  }
+  else {
+    return false;
+  }
+  }
+  
+  
+  function bounceOff(object1,object2){
+    if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2) {
+      object1.velocityX = object1.velocityX * (-10);
+      object2.velocityX = object2.velocityX * (-10);
+      object1.shapeColor="blue";
+      object2.shapeColor="blue";
+    }
+    if (object1.y - object2.y < object2.height/2 + object1.height/2
+      && object2.y - object2.y < object2.height/2 + object1.height/2) {
+        object1.velocityY = object1.velocityY * (-10);
+        object2.velocityY = object2.velocityY * (-10);
+    } 
+  }
+  
